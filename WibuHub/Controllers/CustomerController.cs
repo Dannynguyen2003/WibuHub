31721d214ac97feb53c4a7b679c943d9c56953e2
Nguyen Thanh Danh <danh49003@gmail.com>
2026-01-12 22:55:40 +0700

    new

---
+++ WibuHub/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WibuHub.MVC.Admin.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
